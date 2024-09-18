32d8a4c697b8b791be76bb79117454b311a66f66
Marek324 <hrmajo1@gmail.com>
2024-09-18 02:18:17 +0200

    token header prototype

---
+++ token.h
@@ -0,0 +1,21 @@
+enum TokenType {
+};
+
+enum KeyWordType {
+};
+
+typedef char* dyn_string; // TODO: implement dynamic string
+
+union TokenValue {
+    int int_value;
+    float float_value;
+    dyn_string string_value;
+    KeyWordType keyword;
+};
+    
+
+typedef struct {
+    TokenType type;
+    char* value;
+    int line;
+} Token;
