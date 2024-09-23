af54fb84f78d470f8e34b73cccce6d1c424716aa
Marek324 <hrmajo1@gmail.com>
2024-09-23 21:43:54 +0200

    Added TokenTypes and KeywordTypes

--- include/token.h
+++ include/token.h
@@ -9,11 +9,53 @@
 
 typedef struct dyn_str;
 
-enum TokenType {
-};
+typedef enum {
+    T_KW,
+    T_ID,
+    T_AT,
+    
+    T_INT,
+    T_F64,
+    T_STR,
 
-enum KeyWordType {
-};
+    T_ASGN, T_EQ,
+    T_BANG, T_NEQ,
+    T_LESS, T_LEQ,
+    T_MORE, T_MEQ,
+
+
+    T_PLUS, T_PASGN,
+    T_MINUS, T_MASGN,
+    T_MUL, T_MULASGN,
+    T_DIV, T_DASGN,
+
+    T_LPAREN, T_RPAREN,
+    T_LBRACE, T_RBRACE,
+    T_LBRACKET, T_RBRACKET,
+
+    T_COMMA,
+    T_DOT,
+    T_SEMICOL,
+
+    T_EOF
+} TokenType;
+
+typedef enum {
+    KW_CONST,
+    KW_IF,
+    KW_ELSE,
+    KW_FN,
+    KW_I32,
+    KW_F64,
+    KW_NULL,
+    KW_PUB,
+    KW_RETURN,
+    KW_U8,
+    KW_VAR,
+    KW_VOID,
+    KW_WHILE,
+    KW_IMPORT
+} KeyWordType;
 
 /**
  * @brief Token value structure.
@@ -23,12 +65,13 @@ enum KeyWordType {
  * @param string_value Dynamic string value of token value.
  * @param keyword Keyword type of token value.
  */
-union TokenValue {
+
+typedef union {
     int int_value;
     float float_value;
     dyn_str* string_value;
     KeyWordType keyword;
-};
+} TokenValue;
 
 /**
  * @brief Token structure.
