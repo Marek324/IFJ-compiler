71de0cd747ba77183665ad98c18b5085af07ae54
Marek324 <hrmajo1@gmail.com>
2024-09-24 22:50:38 +0200

    Remade error.h

--- include/error.h
+++ include/error.h
@@ -7,15 +7,6 @@ Header file for error message.
 #ifndef ERROR_H
 #define ERROR_H
 
-void lex_err(const char *fmt, ...);
-void syn_err(const char *fmt, ...);
-void sem_err_3(const char *fmt, ...);
-void sem_err_4(const char *fmt, ...);
-void sem_err_5(const char *fmt, ...);
-void sem_err_6(const char *fmt, ...);
-void sem_err_7(const char *fmt, ...);
-void sem_err_8(const char *fmt, ...);
-void sem_err_9(const char *fmt, ...);
-void sem_err_10(const char *fmt, ...);
+void error_exit(const char *fmt, ...);
 
 #endif // ERROR_H
