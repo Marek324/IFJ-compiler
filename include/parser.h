492811e11629d97a0e02de8abaca5b05ee445543
Adam Vesely <adamvesely2003@gmail.com>
2024-09-29 22:08:03 +0200

    Added return values to parser.h

--- include/parser.h
+++ include/parser.h
@@ -6,4 +6,25 @@
  * @brief Header file for the parser.
  */
 
+#ifndef PARSER_H
+#define PARSER_H
+
+typedef enum return_values {
+    SUCCESS,
+    LEXICAL_ERROR,
+    SYNTAX_ERROR,
+    SEMANTIC_ERROR_DEFINITION,
+    SEMANTIC_ERROR_PARAMETER,
+    SEMANTIC_ERROR_ASSIGNMENT,
+    SEMANTIC_ERROR_EXPRESSION,
+    SEMANTIC_ERROR_COMPATIBILITY,
+    SEMANTIC_ERROR_TYPE,
+    SEMANTIC_ERROR_VARIABLE,
+    SEMANTIC_ERROR_OTHER,
+    INTERNAL_ERROR = 99
+} return_values;
+
+
+#endif
+
 /*** End of file parser.h ***/
