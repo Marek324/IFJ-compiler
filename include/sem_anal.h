2e121266933b0e70a6f3ebe63af1e04e0e7c6881
Adam Vesely <adamvesely2003@gmail.com>
2024-09-30 12:55:15 +0200

    Added semantic analyzer header and c files

---
+++ include/sem_anal.h
@@ -0,0 +1,16 @@
+/**
+ * @file sem_anal.h
+ * 
+ * @author Adam Veselý
+ * 
+ * @brief Header file for the semantic analyzer.
+ */
+
+#ifndef SEM_ANAL_H
+#define SEM_ANAL_H
+
+
+
+#endif
+
+/*** End of file sem_anal.h ***/
---
+++ src/sem_anal.c
@@ -0,0 +1,11 @@
+/**
+ * @file sem_anal.c
+ * 
+ * @author Adam Veselý
+ * 
+ * @brief Main semantic analyzer file.
+ */
+
+#include "../include/sem_anal.h"
+
+/*** End of file sem_anal.c ***/
