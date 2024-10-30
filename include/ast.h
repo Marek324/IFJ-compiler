f90efe86ce76359c68ef019d671977e4b7ba5c4e
StefieS <mikilesiga@gmail.com>
2024-10-30 21:33:55 +0100

    AST_API

--- include/ast.h
+++ include/ast.h
@@ -4,34 +4,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>  
-#include "token.h"  
+#include "error.h"
+#include "token.h"
 
-// Forward declaration for ASTNode
-struct ASTNode;
-typedef struct ASTNode ASTNode;
+typedef enum {
+// Non-terminal nodes (for each rule in P)
+    PROG,               // <prog> -> <prolog> <function_def> <end>
+    PROLOG,             // <prolog> -> const ID = @ import ( <expression> ) ;
+    END,                // <end> -> EOF
+    FUNCTION_DEF,       // <function_def> -> pub fn ID (<param_list>) <type_complete> <block> <function_def> | ε
+    PARAM_LIST,         // <param_list> -> ID : <type_complete> <comma_par_found> | ε
+    COMMA_PAR_FOUND,    // <comma_par_found> -> , <param> | ε
+    PARAM,              // <param> -> ID : <type_complete> <comma_par_found> | ε
+    BLOCK,              // <block> -> { <statement> }
+    STATEMENT,          // <statement> -> <var_declaration> <statement> | ID <ID_found> <statement> | ... | ε
+    VAR_DECLARATION,    // <var_declaration> -> const ID : <type_complete> = <expression> ; | var ID : <type_complete> = <expression> ;
+    IF_STATEMENT,       // <if_statement> -> if ( <expression> ) <optional_value> <block> <else_statement>
+    ELSE_STATEMENT,     // <else_statement> -> else <block> | ε
+    OPTIONAL_VALUE,     // <optional_value> -> | ID | | ε
+    WHILE_LOOP,         // <while_loop> -> while ( <expression> ) <optional_value> <optional_statements> <block> <else_statement>
+    RETURN_STATEMENT,   // <return_statement> -> return <expression> ;
+    EXPRESSION_LIST,    // <expression_list> -> <expression> <comma_expr_found> | ε
+    COMMA_EXPR_FOUND,   // <comma_expr_found> -> , <expression_list2> | ε
+    EXPRESSION_LIST2,   // <expression_list2> -> <expression> <comma_expr_found> | ε
+    TYPE,               // <type> -> i32 | f64 | [ ] u8 | void | bool
+    FOR_LOOP,           // <for_loop> -> for ( <expression> ) <optional_value> <block>
+    OPTIONAL_STATEMENTS,// <optional_statements> -> : <block> | ε
+    ID_FOUND,           // <ID_found> -> = <expression> ; | ( <expression_list> ) | : <while_loop> | . ID ( <expression_list> );
+    TYPE_COMPLETE,      // <type_complete> -> <question_mark> <type>
+    QUESTION_MARK,      // <question_mark> -> ? | ε
+    EXPRESSION          // expession_parser()
+} ASTNodeType;
 
-// Definition of the AST Node structure
-struct ASTNode {
-    Token token;       // The token associated with this AST node
-    ASTNode *left;     // Pointer to the left child node
-    ASTNode *right;    // Pointer to the right child node
-};
 
-ASTNode *initASTNode(Token token);
 
-// Function to add a word token to the AST
-void addWordToAST(ASTNode *ptr, Token token);
+typedef struct ASTNode {
+    ASTNodeType type;
+    struct ASTNode *left;
+    struct ASTNode *right;
+    Token *token;
 
-// Function to add a function token to the AST
-void addFunToAST(ASTNode *ptr, Token token);
+}ASTNode;
 
-// Function to add a line token to the AST
-void addLineToAST(ASTNode *ptr, Token token);
+//ASTRoot globally initialized in case of internal or scanner errors 
+ASTNode *ASTRoot;
 
-// Function to get the start of the last added node
-ASTNode *getBeginningOfLast(ASTNode *ptr);
+void initializeAST();
 
-// Function to get the last added node in the AST
-ASTNode *getLast(ASTNode *ptr);
+ASTNode *nodeCreate(ASTNodeType type, Token *token);
+
+void insertLeft(ASTNode *parent, ASTNode *leftChild);
+
+void insertRight(ASTNode *parent,ASTNode *rightChild);
+
+void freeAST(ASTNode *ptr);
+
 
 #endif // AST_H__
--- src/ast.c
+++ src/ast.c
@@ -1,67 +1,57 @@
-#include "ast.h"
+#include "../include/ast.h"
+
+// Function to initialize the global ASTRoot
+void initializeAST() {
+    ASTRoot = (ASTNode*)malloc(sizeof(ASTNode));
+    if (ASTRoot == NULL) {
+        error_exit(99, "Malloc failed\n");
+    }
+    ASTRoot->type = PROG;  // Initial type
+    ASTRoot->left = NULL;  // No children initially
+    ASTRoot->right = NULL;
+    ASTRoot->token = NULL; 
+}
 
 // Function to create and initialize a new ASTNode
-ASTNode *initASTNode(Token token) {
-    ASTNode *newNode = (ASTNode *)malloc(sizeof(ASTNode));
-    if (newNode == NULL) {
-        return NULL;
+ASTNode * nodeCreate(ASTNodeType type, Token *token) {
+    if (token->type == T_ERROR) {
+        freeAST(ASTRoot);
+        error_exit(token->value.int_value, "Malloc failed\n");
     }
-    newNode->token = token;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+    ASTNode* node = (ASTNode*)malloc(sizeof(ASTNode));
+    if (node == NULL) {
+        freeAST(ASTRoot);
+        error_exit(99, "Malloc failed\n");
+    }
+    node->type = type;
+    node->left = NULL;
+    node->right = NULL;
+    node->token = token;
+    return node;
 }
 
-// Function to traverse to the beginning of the last node in the chain
-ASTNode *getBeginningOfLast(ASTNode *ptr) {
-    while (ptr->right != NULL) {
-        ptr = ptr->right;
-    }
-    return ptr;
+// Functions to link children to their parent <|:)
+void insertLeft(ASTNode* parent, ASTNode* leftChild) {
+    parent->left = leftChild;
 }
 
-// Function to traverse to the last node in the chain
-ASTNode *getLast(ASTNode *ptr) {
-    while (ptr->left != NULL) {
-        ptr = ptr->left;
-    }
-    return ptr;
+void insertRight(ASTNode* parent, ASTNode* rightChild) {
+    parent->right = rightChild;
 }
 
-// Function to add a word node to the AST
-void addWordToAST(ASTNode *ptr, Token token) {
-    ptr = getBeginningOfLast(ptr);
-    if (ptr->left != NULL) {
-        ASTNode *newNode = initASTNode(token);
-        if (newNode == NULL) return;
-        ptr = ptr->left;
-        ptr = getBeginningOfLast(ptr);
-        ptr = getLast(ptr);
-        ptr->left = newNode;
-    }
-    else {
-        addLineToAST(ptr, token);
-    }
-}
+// Function to free the memory allocated for the AST
+void freeAST(ASTNode *ptr) {
+    if (ptr == NULL) return;  //if the node is NULL, do nothing
 
-// Function to add a line node to the AST
-void addLineToAST(ASTNode *ptr, Token token) {
+    // Recursively free the left and right children
+    freeAST(ptr->left);
+    freeAST(ptr->right);
     
-    ASTNode *newNode = initASTNode(token);
-    if (newNode == NULL) return;
-    ptr = getBeginningOfLast(ptr);
-    if (ptr->left != NULL) {
-        ptr = ptr->left;
-        ptr = getBeginningOfLast(ptr->left);
-        ptr->right = newNode;
-    } else {
-        ptr->left = newNode;
+    // Free the token
+    if (ptr->token != NULL) {
+        free(ptr->token);
     }
-}
-
-void addFunToAST(ASTNode *ptr, Token token) {
-    ASTNode *newNode = initASTNode(token);
-    if (newNode == NULL) return;
-    ptr = getBeginningOfLast(ptr);
-    ptr->right = newNode;
+    
+    // Free the current node
+    free(ptr);
 }
