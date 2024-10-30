facb2b88028d5aed69ad9b26a2f81f1c4ab9ddc0
Adam Vesely <adamvesely2003@gmail.com>
2024-10-30 21:25:55 +0100

    Added header file for the expression parser.

---
+++ include/exp_parser.h
@@ -0,0 +1,13 @@
+/*
+file: exp_parser.h
+author: Adam Vesely
+Header file for the expression parser.
+*/
+
+#ifndef EXP_PARSER_H
+#define EXP_PARSER_H
+
+#include "ast.h"
+#include "token.h"
+
+#endif // EXP_PARSER_H
