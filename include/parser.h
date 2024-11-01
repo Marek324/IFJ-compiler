aca28ddbbefb0890a6e59303a7aa88b635578b9c
StefieS <mikilesiga@gmail.com>
2024-11-01 23:40:17 +0100

    more functions

--- include/parser.h
+++ include/parser.h
@@ -3,8 +3,6 @@
 
 #include "ast.h"
 
-
-
 void Parse();
 
 void Prog(Token *token, circ_buff_ptr buffer);
@@ -16,6 +14,20 @@ void FunctionDef(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
 void End(Token *token, ASTNode *ptr, circ_buff_ptr buffer); 
 
 void Expression(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
+
+void ParamList(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
+
+void CommaParFound(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
+
+void TypeComplete(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
+
+void QMark(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
+
+void Type(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
+
+void Block(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
+
+void Statement(Token *token, ASTNode *ptr, circ_buff_ptr buffer);
 
 
 #endif // PARSER__H_
