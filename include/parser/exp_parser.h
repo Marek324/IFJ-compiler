09462596675bd9597019ac435d6d719125e1b04f
Adam Vesely <adamvesely2003@gmail.com>
2024-11-03 01:27:20 +0100

    Added associativity states

--- include/parser/exp_parser.h
+++ include/parser/exp_parser.h
@@ -15,10 +15,14 @@ Header file for the expression parser.
 
 // the associativity of operators
 enum ASOC {
-    
+    L,  /* Lower precedence: "<" -> we push onto the stack (shift) */
+    H,  /* Higher precedence: ">" -> we reduce */
+    EQ, /* Equal precedence: "=" */
+    UN  /* Undefined, causing an error */
 }
-// the priorities of operators
-enum PRIORITY{
+// the indices of the precedence table
+enum PREC_TABLE_INDX{
+    // operators
     UNREACHABLE, /* ".?" postfix operator -> ("a.?" == "a orelse unreachable") -> if "a" is null -> print: "panic: reached unreachable code" return code "57" */
     NEGATE,      /* "!a" */
     MUL_DIV,     /* "*, /" */
@@ -26,7 +30,12 @@ enum PRIORITY{
     ORELSE,      /* "x = a orelse b" -> if a is null x will be "b" with the same data type. Else it will be "a" with the same datatype. */
     RELATION,    /* "<, >, <=, >=, ==, !=" */
     AND,         /* "a and b" */
-    OR           /* "a or b" */
+    OR,          /* "a or b" */
+
+    L_PAR,       /* "(" */
+    R_PAR,       /* ")" */
+    ID,          /* Identifier */
+    END          /* "$" */
 };
 
 ASTNode* parseExpression(Token* token, circ_buff_ptr buffer);
