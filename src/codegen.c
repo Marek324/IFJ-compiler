83813785c5efb16b48e76235b0dea5ce1d9a2c06
Marek324 <hrmajo1@gmail.com>
2024-11-18 16:23:00 +0100

    codegen

--- src/codegen.c
+++ src/codegen.c
@@ -5,6 +5,18 @@ int codegen(ASTNode* root)
 {
     dyn_str *functions = dyn_str_init();
 
+    if (functions == NULL) {
+        fprintf(stderr, "Error: Memory allocation failed\n");
+        return 99;
+    }
+
+    if (root == NULL) {
+        fprintf(stderr, "Error: Root is NULL\n");
+        return 99;
+    }
+
+    root = root->right->left; // root->right is prolog, root->right->left is first function
+    
     PROLOG;
 
     
