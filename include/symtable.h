1fb451ff93c1360d369bc18037bcb94127e06d8a
Marek324 <hrmajo1@gmail.com>
2024-11-18 22:09:54 +0100

    Symtable utils

--- include/symtable.h
+++ include/symtable.h
@@ -1,10 +1,42 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
 
+typedef enum {
+    T_VAR,
+    T_FUN
+} symtable_entry_type;
+
+typedef enum {
+    T_INT,
+    T_FLOAT,
+    T_BOOL,
+    T_STR,
+    T_NULL
+} ret_type;
+
 typedef struct symtable_entry_s{
-    int data;
+    symtable_entry_type entry_type;
+    
+    // common
+    ret_type type;
+    bool isNullable;
+    bool isUsed;
+    bool isChanged;
+
+    //variables
+    bool isConst;
+    bool hasExplicitType;
+    bool isDefined;
+
+    // functions
+    int param_count;
+    bool *param_nullable;
+    ret_type *param_types;
+    bool returnsValue;
+    symtable_node_ptr local_symtable;
 } symtable_entry_t, *symtable_entry_ptr;
 
 typedef struct symtable_node_s{
---
+++ include/symtable_utils.h
@@ -0,0 +1,3 @@
+#include "symtable.h"
+
+print_symtable(symtable_node_ptr tree);
