3e9910962a7e07f61389517fc0e4b3ac51486683
Marek324 <hrmajo1@gmail.com>
2024-11-18 21:32:24 +0100

    nic moc

--- src/main.c
+++ src/main.c
@@ -7,7 +7,12 @@
 int main() {
     circ_buff_ptr buffer = circ_buff_init();
     Parse(buffer);
+
+    // Debug
     print_tree(ASTRoot);
+    
+    // codegen();
+    
     circ_buff_free(buffer);
     freeAST(ASTRoot);
     return 0;
