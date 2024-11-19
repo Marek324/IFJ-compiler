26b16e18bf9b6e2e48a8da5192e5e69a09fd350a
Marek324 <hrmajo1@gmail.com>
2024-11-19 18:36:38 +0100

    for jupi

---
+++ codegen_test/src/for.zig
@@ -0,0 +1,14 @@
+const ifj = @import("ifj24.zig");
+pub fn main() void {
+    const str: []u8 = ifj.string("rete zec");
+    for (str) |temp| {
+        const character: i32 = @as(i32, temp);
+        if (character == 32) {
+            ifj.write("Nemam rad mezery.\n");
+            continue;
+        } else {}
+        ifj.write("Ordinalni hodnota: ");
+        ifj.write(character);
+        ifj.write(".\n");
+    }
+}
