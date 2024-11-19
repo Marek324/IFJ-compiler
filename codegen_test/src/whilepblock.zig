bb50572d6d3b9c315e52814ade49d919b3052db8
Marek324 <hrmajo1@gmail.com>
2024-11-19 17:52:35 +0100

    a

---
+++ codegen_test/src/whilepblock.zig
@@ -0,0 +1,9 @@
+const ifj = @import("ifj24.zig");
+
+pub fn main() void {
+    var i: i32 = 0;
+    while (i < 10) : ({i = i + 1;}) {
+        ifj.write(i); ifj.write("\n");
+        i = i + 1;
+    }
+}
