71724a963bc723c29381d8649e01a66e2ef17c76
Marek324 <hrmajo1@gmail.com>
2024-11-21 22:49:58 +0100

    abcd

---
+++ include/codegen_priv.h
@@ -0,0 +1,16 @@
+#include "codegen.h"
+
+void prog(ASTNode *root);
+void function_def(ASTNode *node);
+void param_list(ASTNode *node);
+void statement(ASTNode *node);
+void var_dec(ASTNode *node);
+void id_statement(ASTNode *node);
+void if_statement(ASTNode *node);
+void while_loop(ASTNode *node);
+void for_loop(ASTNode *node);
+void return_statement(ASTNode *node);
+void break_statement(ASTNode *node);
+void continue_statement(ASTNode *node);
+void expression(ASTNode *node);
+void func_call(ASTNode *node);
