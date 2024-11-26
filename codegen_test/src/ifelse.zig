bf568977f2ecb414ad0703f9c0c18ff1a2015b1d
Marek324 <hrmajo1@gmail.com>
2024-11-26 19:04:40 +0100

    if hura

--- codegen_test/src/ifelse.zig
+++ codegen_test/src/ifelse.zig
@@ -4,7 +4,13 @@ pub fn main() void {
   const y = 12;
   const x = 3;
   if(x > y) {
-    ifj.write(x); 
+    if (true and true){
+        if(true){
+            if(true == true){
+                ifj.write(x);
+            }
+        }
+    } 
   } else {
     ifj.write(y); 
   }
