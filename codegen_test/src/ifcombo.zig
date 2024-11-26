1e0a6fec6feaff5debe21386631ee8b6cc9c3d9f
Marek324 <hrmajo1@gmail.com>
2024-11-26 11:28:25 +0100

    test src

---
+++ codegen_test/src/ifcombo.zig
@@ -0,0 +1,24 @@
+const ifj = @import("ifj24.zig");
+
+pub fn main() void {
+  const y = 12;
+  const x = 3;
+  const z = 4;
+  if(x > y) {
+    ifj.write(x); 
+  } else if(z > y) {
+    ifj.write(z);
+  } else {
+    ifj.write(y); 
+  }
+  ifj.write("\n");
+
+  const a = ifj.readi32();
+  if (a) |b|{
+    ifj.write(b);
+  }
+  else {
+    ifj.write("nic");
+  }
+  ifj.write("\n");
+}
