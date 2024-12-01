79255962829634849b7477960698d4c5c59b260e
Marek324 <hrmajo1@gmail.com>
2024-12-01 22:13:38 +0100

    test fix

--- codegen_test/src/test6.zig
+++ codegen_test/src/test6.zig
@@ -3,13 +3,12 @@ const ifj = @import("ifj24.zig");
 pub fn main() void {
 
     const a = ifj.string("abcd");
-        ifj.write(foo(a));
+    ifj.write(foo(a));
     ifj.write("\n");
 }
 
-pub fn foo(a:[]u8) []u8{
+pub fn foo(a:[]u8) i32{
     const b = 12;
-    const c = 0-10;
     return (b - 1) * 8 + ifj.ord(a, 0) - ifj.ord(ifj.string("a"), 0);
     
 }
